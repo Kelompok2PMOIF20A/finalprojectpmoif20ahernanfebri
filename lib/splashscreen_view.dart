3e48abbb7348a5cd73a37bb3745b04e8e3b40116
Hernan Febri <hernanfebri@gmail.com>
2022-06-21 02:43:30 +0700

    Menambahkan SplashScreen dan Homeview

---
+++ lib/splashscreen_view.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:finalprojectpmo/splashscreen_view.dart';
+import 'dart:async';
+
+import 'home_view.dart';
+
+class SplashScreenPage extends StatefulWidget {
+  @override
+  _SplashScreenPageState createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    startSplashScreen();
+  }
+
+  startSplashScreen() async {
+    var duration = const Duration(seconds: 5);
+    return Timer(duration, () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) {
+          return HomePage();
+        }),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff329cef),
+      body: Center(
+        child: Image.asset(
+          "assets/logo.png",
+          width: 1000.0,
+          height: 700.0,
+        ),
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,6 +14,8 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
+    //import 'package:flutter_splashscreen/main.dart';
+    //await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
